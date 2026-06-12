b325ead06a63bdcb75cde062a69e8f9e0340e540
jaivanshchawla <jaivanshmuj@gmail.com>
2026-06-12 17:10:42 +0530

    perf(hooks): add sort=pushed to repos fetch for better ordering

--- src/hooks/useGitHubRepos.js
+++ src/hooks/useGitHubRepos.js
@@ -4,6 +4,6 @@ import { githubFetch } from '../lib/github'
 export const useGitHubRepos = () =>
   useQuery({
     queryKey: ['repos'],
-    queryFn: () => githubFetch('/user/repos?per_page=100&type=all'),
+    queryFn: () => githubFetch('/user/repos?per_page=100&type=all&sort=pushed'),
     enabled: !!localStorage.getItem('dailies_pat'),
   })
