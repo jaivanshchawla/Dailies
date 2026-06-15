095af995d4dd5afb7dbfdda2a5c24616a0ed3ab3
jaivanshchawla <jaivanshmuj@gmail.com>
2026-06-15 22:07:13 +0530

    feat(config): add darkMode selector to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: ['selector', '[data-theme="dark"]'],
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {},
