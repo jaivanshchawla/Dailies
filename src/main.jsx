524d5ac3a87d78b2874a99b37ba49d740a2501d6
jaivanshchawla <jaivanshmuj@gmail.com>
2026-06-15 22:09:41 +0530

    feat(app): wrap app in ThemeProvider for dark/light theme support

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ClerkProvider } from '@clerk/clerk-react'
+import { ThemeProvider } from './contexts/ThemeContext'
 import App from './App'
 import './index.css'
 
@@ -19,9 +20,11 @@ const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
-      <QueryClientProvider client={queryClient}>
-        <App />
-      </QueryClientProvider>
+      <ThemeProvider>
+        <QueryClientProvider client={queryClient}>
+          <App />
+        </QueryClientProvider>
+      </ThemeProvider>
     </ClerkProvider>
   </React.StrictMode>
 )
