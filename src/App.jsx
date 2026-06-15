8d94e49dd3f8b313aa4b9b92b23fe73319db1aad
jaivanshchawla <jaivanshmuj@gmail.com>
2026-06-15 22:10:28 +0530

    feat(app): add animated gradient blob background behind glass UI

--- src/App.jsx
+++ src/App.jsx
@@ -2,5 +2,12 @@ import React from 'react'
 import Feed from './pages/Feed'
 
 export default function App() {
-  return <Feed />
+  return (
+    <>
+      <div className="app-background">
+        <div className="blob-3" />
+      </div>
+      <Feed />
+    </>
+  )
 }
